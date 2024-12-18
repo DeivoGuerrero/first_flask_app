19c9235e44d6cab9c07d59ca544324d54e702218
David Guerrero <deivo.guerrero@gmail.com>
2024-12-18 12:56:50 -0500

    clase 233, manejo de for dentro de archivo jinja2 y obtención de link con paso de parámetros

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ posts = {
 
 @app.route('/')
 def home():
-    return 'Hello, world'
+    return render_template('home.jinja2', posts=posts)
 
 @app.route('/post/<int:post_id>') # /post/0
 def post(post_id):
